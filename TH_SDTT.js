f9518c3daabecaec7c6f4801690b6b48881cfb7a
HieuT23511 <hieut23511@gmail.com>
2023-03-08 22:43:44 +0700

    complete TH_SDTT

---
+++ TH_SDTT.js
@@ -0,0 +1,8 @@
+let inpWidth;
+let inpHeight;
+inpWidth = prompt('Nhap vao chieu dai');
+inpHeight = prompt('Nhap vao chieu rong');
+let width = parseInt(inpWidth);
+let height = parseInt(inpHeight);
+let area = width*height;
+document.write('Dien tich hinh chu nhat la ' +area);
